81bb2c83ccfa59fc4b51373683b02b53acdc3ce0
WithoutAName25 <withoutaname25@gmail.com>
2021-08-03 00:23:53 +0200

    All fields, methods & parameters are nonnull by default

---
+++ src/main/java/withoutaname/mods/withoutio/datagen/package-info.java
@@ -0,0 +1,10 @@
+@FieldsAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package withoutaname.mods.withoutio.datagen;
+
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
